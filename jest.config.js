e7187f9ca3a365d2d3a21035e3d3860f2d15b64e
Derek Kershner <dkershner@cbtnuggets.com>
2020-07-24 12:12:59 -0700

    Reinstalled testing-library/user-event

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     testPathIgnorePatterns: ['./node_modules/', './demo-site', 'dist'],
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-    coveragePathIgnorePatterns: ['node_modules'],
+    coveragePathIgnorePatterns: ['node_modules', 'src/index.ts'],
     globals: {
         // we must specify a custom tsconfig for tests because we need the typescript transform
         // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
